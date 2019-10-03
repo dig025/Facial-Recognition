e471be3b1d6e936c3bafb00664eac0cb96881ce5
Diego Gomez <wintermute@pop-os.localdomain>
2019-10-02 18:08:30 -0700

    started process_local.py

--- project_1/process_local.py
+++ project_1/process_local.py
@@ -13,11 +13,19 @@ Now modify this code to process a local image and do the following:
 All the above steps should be in one function called process_image()
 """
 
-# TODO: Import OpenCV
-
+# Done: Import OpenCV
+import cv2
 
 # TODO: Edit this function
-def process_image():
+def process_image(filename):
+    img = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+    #cv2.imshow("image",img)
+    #cv2.waitKey(0)
+    
+    img = cv2.resize(img, (img.shape[1]/2, img.shape[0]/2))
+    #cv2.rectangle(img, Point(0,0), Point(img.shape[0],img.shape[1]), Scalar(255,255,255))
+    cv2.imwrite("geisel-bw-rectangle.jpg",img)
+
     return
 
 # Just prints 'Hello World! to screen.
@@ -28,6 +36,11 @@ def hello_world():
 # TODO: Call process_image function.
 def main():
     hello_world()
+    process_image("geisel.jpg") 
+    new_img = cv2.imread("geisel-bw-rectangle.jpg", cv2.IMREAD_GRAYSCALE)
+
+    cv2.imshow("image",new_img)
+    cv2.waitKey(0)
     return
 
 
