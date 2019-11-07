def2e2840ea946ae503ade565ae8f383dd56946b
Diego Gomez <dig025@ucsd.edu>
2019-11-06 17:24:23 -0800

    updated train

--- project_3/train_vgg.py
+++ project_3/train_vgg.py
@@ -22,8 +22,8 @@ import random
 
 IMG_H, IMG_W, NUM_CHANNELS = 224, 224, 3
 MEAN_PIXEL = np.array([104., 117., 123.]).reshape((1, 1, 3))
-TRAIN_DIR = '../data/train'  # TODO
-VAL_DIR = '../data/validation'  # TODO
+TRAIN_DIR = '../Data/Train'  # DONE
+VAL_DIR = '../Data/Validation'  # DONE
 NUM_EPOCHS = 5  # TODO
 BATCH_SIZE = 16
 NUM_CLASSES = 20  # TODO
@@ -32,20 +32,26 @@ NUM_CLASSES = 20  # TODO
 def load_model():
     # TODO: use VGG16 to load lower layers of vgg16 network and declare it as base_model
     # TODO: use 'imagenet' for weights, include_top=False, (IMG_H, IMG_W, NUM_CHANNELS) for input_shape
-
+    base_model = VGG16(include_top=False, weights='imagenet', input_tensor=None, input_shape=(IMG_H, IMG_W, NUM_CHANNELS), pooling=None, classes=NUM_CLASSES)
+	
     print('Model weights loaded.')
     base_out = base_model.output
     # TODO: add a flatten layer, a dense layer with 256 units, a dropout layer with 0.5 rate,
     # TODO: and another dense layer for output. The final layer should have the same number of units as classes
+    base_out = Flatten()(base_out)
+    base_out = Dense(256)(base_out)
+    base_out = Dropout(.5)(base_out)
+    base_out = Dense(NUM_CLASSES)(base_out)
 
-    model = Model(inputs=base_model.input, outputs=predictions)
-    print 'Build model'
+    model = Model(inputs=base_model.input, outputs=base_out)
+    print('Build model')
     model.summary()
 
     # TODO: compile the model, use SGD(lr=1e-4,momentum=0.9) for optimizer, 'categorical_crossentropy' for loss,
     # TODO: and ['accuracy'] for metrics
+    model.compile(optimizers.SGD(lr=1e-4, momentum=.9), loss='categorical_crossentropy',metrics=['accuracy'])
 
-    print 'Compile model'
+    print('Compile model')
     return model
 
 
@@ -59,7 +65,7 @@ def load_data(src_path):
         image_path_list += sorted(glob.glob(os.path.join(class_path, '*jpg')))
     random.shuffle(image_path_list)
     num_images = len(image_path_list)
-    print '-- This set has {} images.'.format(num_images)
+    print('-- This set has {} images.'.format(num_images))
     X = np.zeros((num_images, IMG_H, IMG_W, NUM_CHANNELS))
     Y = np.zeros((num_images, 1))
     # read images and labels
@@ -77,18 +83,20 @@ def load_data(src_path):
 def main():
     # make model
     model = load_model()
-    print 'VGG16 created\n'
+    print('VGG16 created\n')
 
     # read train and validation data and train the model for n epochs
-    print 'Load train data:'
+    print('Load train data:')
     X_train, Y_train = load_data(TRAIN_DIR)
-    print 'Load val data:'
+    print('Load val data:')
     X_val, Y_val = load_data(VAL_DIR)
     # TODO: Train model
+    model.fit(X_train, Y_train, batch_size=BATCH_SIZE, epochs=NUM_EPOCHS, validation_data=(X_val, Y_val))
 
     # TODO: Save model weights
+    model.save('../model.h5')
 
-    print 'model weights saved.'
+    print('model weights saved.')
     return
 
 
